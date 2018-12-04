37327d13c8ace134d95c77ed68f27911dde02410
Bodrug <bodrug1990@gmail.com>
2018-12-04 20:43:57 +0200

    Merge branch 'master' of https://github.com/Bodrug/hillel
    # Conflicts:
    #	src/ua/hillel/bodrug/lesson2/Task6.java

--- src/ua/hillel/bodrug/lesson5/ComplexNumber.java
+++ src/ua/hillel/bodrug/lesson5/ComplexNumber.java
@@ -1,4 +1,47 @@
 package ua.hillel.bodrug.lesson5;
 
 public class ComplexNumber {
+    final double a;
+    final double b;
+
+    public ComplexNumber(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+    public ComplexNumber add(ComplexNumber c){
+        ComplexNumber complex = new ComplexNumber(a + c.a, b + c.b);
+        return complex;
+    }
+
+    public ComplexNumber subtract(ComplexNumber c){
+        ComplexNumber complex = new ComplexNumber(a - c.a, b-c.b);
+        return complex;
+    }
+    public ComplexNumber divide (ComplexNumber c){
+        ComplexNumber complex = new ComplexNumber(((a*c.a + b*c.b)/exp(c.a,c.b)),(b*c.a-a*c.b)/exp(c.a,c.b));
+        return  complex;
+    }
+
+    public double exp (double x, double y){
+        return x*x + y*y;
+    }
+
+    public ComplexNumber mult(ComplexNumber c){
+        ComplexNumber complex = new ComplexNumber((a*c.a-b*c.b),(a*c.b+b*c.a));
+        return complex;
+    }
+    public static void main(String[] args) {
+        ComplexNumber complex = new ComplexNumber(2,3);
+        ComplexNumber complex1 = new ComplexNumber(1,2);
+        ComplexNumber complexAdd = complex.add(complex1);
+        System.out.println("complex = " + complex.a +"+" + complex.b + "*i");
+        System.out.println("complex1 = " + complex1.a + "+" +complex1.b + "*i");
+        System.out.println("complexAdd = " + complexAdd.a + "+" +complexAdd.b + "*i");
+        ComplexNumber complexSub = complex.subtract(complex1);
+        System.out.println("complexSub = " + complexSub.a + "+" +complexSub.b + "*i");
+        ComplexNumber complexDiv = complex.divide(complex1);
+        System.out.println("complexDiv = " + complexDiv.a + "+" +complexDiv.b + "*i");
+        ComplexNumber complexMult = complex.mult(complex1);
+        System.out.println("complexMult = " + complexMult.a + "+" +complexMult.b + "*i");
+    }
 }
